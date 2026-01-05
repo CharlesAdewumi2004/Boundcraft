c89006c29e7721c2513310917e40c3d43e3ff783
CharlesAdewumi2004 <charlesadewumi2004@gmail.com>
2026-01-05 16:29:33 +0000

    added different tags for gallop search

--- include/hybridSearch/policy.hpp
+++ include/hybridSearch/policy.hpp
@@ -14,13 +14,22 @@ namespace hybridSearch::policy
         static constexpr std::size_t threshold = Threshold;
     };
 
-    struct galloping final
-    {
-    };
-
-    template <std::size_t Threshold>
+    template <class Search_Policy, class Gallop_Start>
     struct galloping_binary_linear_hybrid final
     {
-    };
+        using search_policy = Search_Policy;
+        using gallop_start = Gallop_Start;
+    };    
 
+}
+
+namespace hybridSearch::Policy::GallopPolicy{
+    struct start_front final{};
+    struct start_back final{};
+    struct start_middle final{};
+
+    template<std::size_t Last_Searched>
+    struct start_last_searched final{
+        static constexpr std::size_t last_searched = Last_Searched;
+    };
 }
